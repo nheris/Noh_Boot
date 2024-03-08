cbc31961c31ffa73ea9bf9b2382b6056f39ba1a6
Noh <arark057@gmail.com>
2024-03-08 10:17:43 +0900

    interceptor

--- src/main/java/com/winter/app/interceptors/TestInterceptor.java
+++ src/main/java/com/winter/app/interceptors/TestInterceptor.java
@@ -1,5 +1,38 @@
 package com.winter.app.interceptors;
 
-public class TestInterceptor {
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class TestInterceptor implements HandlerInterceptor{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		log.info("=============== POST ===================");
+		
+		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		log.info("=============== post ===================");
+		
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		log.info("=============== after ===================");
+	}
 }
