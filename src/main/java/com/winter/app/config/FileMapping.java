9bd6992f480b152f42bfc71afab3c843f62007b7
Noh <arark057@gmail.com>
2024-03-12 17:47:17 +0900

    1234

--- src/main/java/com/winter/app/config/FileMapping.java
+++ src/main/java/com/winter/app/config/FileMapping.java
@@ -6,13 +6,15 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-@Configuration //***-context.xml
+@Configuration //***-context.xml	//
 public class FileMapping implements WebMvcConfigurer{
 	
 	@Value("${app.upload.url}")
-	private String urlPath; //  /files/**  요청들러오면 컨트롤러 가지말고 밑 주소에서 찾ㅇ
+	private String urlPath; //  /files/**  요청들러오면 컨트롤러 가지말고 filePath 주소에서 찾기
 	@Value("${app.upload.base}")
 	private String filePath; //  D://upload/
+	//ex)files/notice/파일명 -> D://upload/notice/파일명 으로 가서 찾음
+	
 	
 	@Override
 	//외부자원으로 ??
@@ -20,7 +22,7 @@ public class FileMapping implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		//addResourceLocations 메서드를 이용하여 실제 파일이 있는 경로를 지정
 		//<resources mapping="/resources/**" location="/resources/" />
-		registry.addResourceHandler(urlPath)
+		registry.addResourceHandler(urlPath) 
 				.addResourceLocations(filePath);
 	}
 }
